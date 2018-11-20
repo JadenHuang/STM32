842276958b38ea0687609408cbe27f20471011fe
JadenHuang <jianxing217@126.com>
2018-11-20 19:07:55 +0800

    添加时钟项目

---
+++ STM32时钟项目/User/subfunction.h
@@ -0,0 +1,13 @@
+#ifndef __SUBFUNCTION_H
+#define __SUBFUNCTION_H	 
+
+#include "sys.h"
+#include "delay.h"
+#include "rtc.h"
+#include "TM1640.h"
+#include "delay.h"
+
+
+void Time_display(void);
+
+#endif
